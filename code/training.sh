13b14b30232cab374e43e293af0e8785e9eb8ff5
Vikas Raunak <vyraun@gmail.com>
2020-03-28 16:45:09 -0400

    prepare the training script

---
+++ code/training.sh
@@ -0,0 +1,2 @@
+mkdir ../models/evpi
+python -u train.py --data_path ../../clarification_questions_dataset/data --model_save_path ../models/evpi/model.pk --vocab_file ../../clarification_questions_dataset/aux/vocab.pkl
