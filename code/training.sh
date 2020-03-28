82b967942256ea1097f64a7b71201ec76fddd55a
Vikas Raunak <vyraun@gmail.com>
2020-03-28 18:30:34 -0400

    log experiments

--- code/training.sh
+++ code/training.sh
@@ -1,2 +1,3 @@
-mkdir ../models/evpi
-python -u train.py --data_path ../../clarification_questions_dataset/data --model_save_path ../models/evpi/model.pk --vocab_file ../../clarification_questions_dataset/aux/vocab.pkl
+experiment=${1?Error: no experiment name given}
+mkdir ../models/$experiment
+python -u train.py --data_path ../../clarification_questions_dataset/data --model_save_path ../models/$experiment/model.pk --vocab_file ../../clarification_questions_dataset/aux/vocab.pkl
