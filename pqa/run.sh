d98443083490aff2ab0566cba4de3857a2df479c
Vikas Raunak <vyraun@gmail.com>
2020-03-29 19:10:46 -0400

    fixing test script for pqa

--- pqa/run.sh
+++ pqa/run.sh
@@ -1,4 +1,4 @@
 experiment=${1?Error: no experiment name given}
 mkdir ../models/$experiment
 python -u train.py --max_epochs 1 --valid_iter 300 --data_path ../../clarification_questions_dataset/data --model_save_path ../models/$experiment/model.pk --vocab_file ../../clarification_questions_dataset/aux/vocab.pkl
-python -u test.py --data_path ../../clarification_questions_dataset/data --model_path ../models/$experiment/model.pk --vocab_file ../../clarification_questions_dataset/aux/vocab.pkl
+python -u test.py --data_path ../../clarification_questions_dataset/data --model_path ../models/$experiment/model.pk
